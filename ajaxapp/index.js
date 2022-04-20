d271739ffc90eb0cf32882d884ef29d4ee0b9b33
masanaito-bita <masana_ito@bita.jp>
2022-04-20 17:34:42 +0900

    [Ajax] 不要な記述を削除

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,14 +1,3 @@
-// CSSセレクタを使ってDOMツリー中のh2要素を取得
-const heading = document.querySelector("h2");
-// h2に含まれるテキストコンテンツを取得
-const headingText = heading.textContent;
-
-// button要素を作成
-const button = document.createElement("button");
-button.textContent = "Push Me!";
-// body要素の子要素としてbuttonを挿入する
-document.body.appendChild(button);
-
 /**
  * GithubのユーザーIDからユーザー情報を取得する
  * 
