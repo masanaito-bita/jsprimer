b4801dcf2ad49d35c48284a6a6ab600c39d1be1e
masanaito-bita <masana_ito@bita.jp>
2022-04-20 16:11:23 +0900

    [Ajax] GithubのUserIdをからユーザーデータを取得する機能を作成
    エラーハンドリングを作成

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -8,3 +8,19 @@ const button = document.createElement("button");
 button.textContent = "Push Me!";
 // body要素の子要素としてbuttonを挿入する
 document.body.appendChild(button);
+
+// Fetch API ページ全体を再読み込みすることなくURLからデータ取得が可能
+const userId = "masanaito-bita";
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+        // レスポンスの受け取り
+        console.log(response.status); // 200
+        return response.json().then(userInfo => {
+            // JSONパースされたオブジェクトが渡される
+            console.log(userInfo);
+        });
+    }).catch(error => {
+        // NetworkErrorオブジェクトでPromiseが返される
+        // -> thenメソッドの第二引数 or catchメソッドのコールバック関数が呼び出される
+        console.error(error);
+    });
